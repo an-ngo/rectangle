910ec13c9338949f34ad32853faace0c59060bf7
an.ngo <phowflowf135@gmail.com>
2021-09-21 17:22:44 +0700

    m

---
+++ src/accessmodifier/TestCircle.java
@@ -0,0 +1,9 @@
+package accessmodifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(1.2);
+        circle.getArea();
+        circle.getRadius();
+    }
+}
